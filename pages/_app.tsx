9a1f1f3a6bbe495db19204107f180a1e487ff089
Michael Ayeni <thecodingpastor@gmail.com>
2023-09-23 18:19:00 +0100

    updated gtm env in cloud

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import { Poppins } from "@next/font/google";
 import type { AppProps } from "next/app";
 import Layout from "../layout/Layout";
 import RouteLoading from "../components/loaders/RouteLoading";
-// import GTMLayout from "../components/shared/GTM";
 import TagManager, { TagManagerArgs } from "react-gtm-module";
 import { useEffect } from "react";
 
@@ -14,7 +13,6 @@ export const poppins = Poppins({
   weight: ["300", "600", "700"],
   display: "swap",
   subsets: ["latin"],
-  // variable: "--font-poppins",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -36,14 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
           content="The Lagos Bible Conference (LBC) is the annual flagship conference of Sovereign Grace Bible Church (and partners) dedicated to promoting the Bible as God's inerrant and infallible Word through a faithful exposition of it."
         />
         <link rel="icon" href="/logo.webp" />
-
-        {/* <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NJV6HW4K');`}</script> */}
       </Head>
-      {/* <GTMLayout /> */}
       <RouteLoading />
       <Layout className={poppins.className}>
         <Component {...pageProps} />
