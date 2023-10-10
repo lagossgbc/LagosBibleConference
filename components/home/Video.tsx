8d38c4ae638ad180bb8241b55a28869cb98cd128
Michael Ayeni <thecodingpastor@gmail.com>
2023-10-10 13:52:19 +0100

    added plain video

--- components/home/Video.tsx
+++ components/home/Video.tsx
@@ -1,35 +1,16 @@
-import { useState } from "react";
-
-import Image from "next/image";
 import classes from "./Video.module.scss";
 
 const Video = () => {
-  const [Mode, setMode] = useState("image");
-  let content;
-
-  if (Mode === "image") {
-    content = (
-      <figure onClick={() => {}} className={classes.Figure}>
-        <Image
-          src="https://websiteimages.nyc3.cdn.digitaloceanspaces.com/lbc/video-placeholder.webp"
-          alt="Video place-holder"
-          fill
-        />
-      </figure>
-    );
-  } else {
-    content = (
-      <div className={classes.Container}>
-        <div className={classes.Inner}>
-          <video
-            src="https://websiteimages.nyc3.cdn.digitaloceanspaces.com/lbc/intro-lbc.mp4"
-            controls
-          ></video>
-        </div>
+  return (
+    <div className={classes.Container}>
+      <div className={classes.Inner}>
+        <video
+          src="https://websiteimages.nyc3.cdn.digitaloceanspaces.com/lbc/intro-lbc.mp4"
+          controls
+        ></video>
       </div>
-    );
-  }
-  return content;
+    </div>
+  );
 };
 
 export default Video;
